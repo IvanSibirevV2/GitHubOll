a2cbd6b3edbe0b1ae98112dca0e4488cbd3dc529
qwe <qwe>
2023-01-25 14:49:44 +0300

    0000

--- YP.01.01.bat
+++ YP.01.01.bat
@@ -1,6 +1,6 @@
 @echo off
 cd %~dp0
 echo %cd%
-git clone --depth 1 https://github.com/IvanSibirevV2/CSharp_BrainFuck.git
+git clone --depth 1 https://github.com/IvanSibirevV2/YP.01.01.git
 TIMEOUT /T 10
 exit /b
